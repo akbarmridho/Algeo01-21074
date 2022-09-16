bae8f4b200fbc8fa06a6614421c12bab1cefe626
Akbar Maulana Ridho <akbarmaulanaridho1@gmail.com>
2022-09-16 14:15:52 +0700

    ADT MatrixSquare

--- src/matrix/MatrixSquare.java
+++ src/matrix/MatrixSquare.java
@@ -5,15 +5,35 @@ public class MatrixSquare extends Matrix {
         super(size, size, contents);
     }
 
+    /*
+     * Menghitung determinan suatu matriks dengan metode minor-kofaktor
+     * Fungsi ini fungsi rekursif
+     */
     public double getDeterminant() {
-        // gunakan dengan metode minor-kofaktor
-        // dan rekursif
+        // cek baris/ kolom dengan nol terbanyak dan lakukan perhitungan dari sana
+        // cek jika ada baris/ kolom yang full 0, jika ya, langsung kembalikan nol
     }
 
+    /*
+     * Memperoleh nilai kofaktor dari suatu matriks (minimal matriks 2x2)
+     */
+    public double getCofactor(MatrixSquare matrix) {
+
+    }
+
+    /*
+     * Mengembalikan adjoint dari suatu matriks
+     * Menggunakan metode minor-kofaktor, lalu di-transpose
+     * 
+     * Berdakan case saat matriks 2x2 dengan matriks ukuran nxn dengan n>=3
+     */
     public MatrixSquare getAdjoint() {
         //
     }
 
+    /*
+     * Mengembalikan invers matriks menggunakan metode minor-kofaktor
+     */
     public MatrixSquare getInverse() {
         //
     }
